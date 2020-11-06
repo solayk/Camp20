a1c23b3b9e0439b7eb5036e3e12790e697c0d5a2
kosmo20 <kosmo20@DESKTOP-F3C3SVG>
2020-11-06 17:44:17 +0900

    RegisterDAO 수정

--- reservation/src/reservation/model/vo/RegisterDAO.java
+++ reservation/src/reservation/model/vo/RegisterDAO.java
@@ -3,6 +3,7 @@ package reservation.model.vo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import reservation.model.vo.CustomerVO;
@@ -71,6 +72,55 @@ public class RegisterDAO {
 		
 	}
 	
-	
+	//
+	//
+	//
+	public int chk_idpw(String id, String pw) throws Exception {
+		
+		String sql1 = "SELECT * \r\n" + 
+				" FROM CUSTOMER\r\n" + 
+				" WHERE customer_id = ? \r\n" 
+				;
+		
+		PreparedStatement st1 = con.prepareStatement(sql1);
+		ResultSet rs1;
+		
+		st1.setString(1, id);
+		
+		rs1 = st1.executeQuery();
+		
+		String chk_id_only = null;
+		
+		while(rs1.next()) {
+			chk_id_only = rs1.getString("CUSTOMER_ID");
+		}
+		
+		// 아이디 존재여부 확인 => 만일 chk_id_only가 null이면 없는 id 
+		if(chk_id_only != null) {
+			System.out.println("아이디 존재함");
+			
+			String sql2 = "SELECT * \r\n" + 
+					" FROM CUSTOMER\r\n" + 
+					" WHERE customer_id = ? AND customer_pw = ? \r\n" 
+					;
+			PreparedStatement st2 = con.prepareStatement(sql2);
+			ResultSet rs2;
+			
+			st2.setString(1, id);
+			st2.setString(2, pw);
+			
+			rs2 = st2.executeQuery();
+			
+			String chk_both = null;
+			
+			while(rs2.next()) {
+				chk_both = rs2.getString("CUSTOMER_ID");
+			}
+				if(chk_both != null) return 1; // 아이디 비밀번호 일치 = 로그인 성공
+				else return 3;	// 에러: 아이디 비밀번호 불일치			
+		}
+		else return 2; // 에러: 아이디 존재하지 않음
+		
+	}
 }
 
